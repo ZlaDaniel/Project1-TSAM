b84b27b1e29b07dc7ed96ec75140e5e126247702
ZlaDaniel <danni539953@gmail.com>
2021-09-05 22:28:48 +0000

    Add files via upload

---
+++ Dæmi3/clientV2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // Creating socket
+    int socket1 = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket1 < 0)
+    {
+        cerr << "Error: " << hstrerror(errno) << endl;
+        exit(1);
+    }
+ 
+    // Filling in server IP address and Port number
+    struct sockaddr_in server;
+    bzero(&server, sizeof(server));
+ 
+    char* hostIP;
+    if (argc > 1)
+        hostIP = argv[1];
+ 
+    server.sin_family = AF_INET;
+    short hostPort = 3000;
+    if (argc >= 3)
+        hostPort = (short)atoi(argv[2]);
+    server.sin_port = htons(hostPort);
+ 
+    // Connecting to the remote server
+    int res = connect(socket1, (struct sockaddr*) &server, sizeof(server));
+    if (res < 0)
+    {
+        cerr << "Error: " << hstrerror(errno) << endl;
+        exit(1);
+    }
+ 
+    cout << "Connected" << endl;
+    
+    // Running the command loop
+    string command;
+    while(true) {
+        cout << "Write a command you wish to use! (write 'exit' to quit)" << endl;
+        getline(cin, command);
+        if (command == "exit")
+            break;
+        char* array = new char[command.length() + 1];
+        strcpy(array, command.c_str());
+        write(socket1, array, 20);
+        char buffer[2048] = {0};
+        res = read(socket1, buffer, 2047);
+        if (res < 0)
+        {
+            cerr << "Error: " << hstrerror(errno) << endl;
+            exit(1);
+        }
+        cout << buffer;
+    }
+    close(socket1);
+    return 0;
+}
